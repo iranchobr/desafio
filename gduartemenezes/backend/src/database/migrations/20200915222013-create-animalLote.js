5c0b0aa2d1ee4d76d1f6fdcaf4a403981bfe4ad5
Gustavo Duarte <gduartemenezes@gmail.com>
2020-09-15 19:39:10 -0300

    Adiciona migration animalLote

---
+++ gduartemenezes/backend/src/database/migrations/20200915222013-create-animalLote.js
@@ -0,0 +1,60 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('animals', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+      },
+      dt_entrada: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      dt_saida: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      dt_ultima_movimentacao: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      ic_bezerro: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        default: false,
+      },
+      fk_id_lote: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          key: 'id',
+          model: 'lotes',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      fk_id_animal: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          key: 'id',
+          model: 'animals',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('animals');
+  },
+};
