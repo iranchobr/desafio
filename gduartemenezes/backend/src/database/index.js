682b78d12fbbeeadf2a0d38d9263df7cd5c41251
Gustavo Duarte <gduartemenezes@gmail.com>
2020-09-15 20:42:47 -0300

    Registra modelo em dbConnection

--- gduartemenezes/backend/src/database/index.js
+++ gduartemenezes/backend/src/database/index.js
@@ -4,8 +4,9 @@ import databaseConfig from '../config/database';
 import Pessoa from '../app/models/Pessoa';
 import Animal from '../app/models/Animal';
 import Lote from '../app/models/Lote';
+import AnimalLote from '../app/models/AnimalLote';
 
-const models = [Pessoa, Animal, Lote];
+const models = [Pessoa, Animal, Lote, AnimalLote];
 
 class Database {
   constructor() {
