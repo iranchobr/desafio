cf656a27adb197b7207d29b5acb381f2cec4df96
Gustavo Duarte <gduartemenezes@gmail.com>
2020-09-15 16:26:40 -0300

    Adiciona rotas /animais

--- gduartemenezes/backend/src/routes.js
+++ gduartemenezes/backend/src/routes.js
@@ -1,13 +1,22 @@
 import { Router } from 'express';
 
 import PessoaController from './app/controllers/PessoaController';
+import AnimalController from './app/controllers/AnimalController';
 
 const routes = new Router();
 
+// rotas /pessoas
 routes.post('/pessoas', PessoaController.criar);
 routes.get('/pessoas', PessoaController.listarTodos);
 routes.put('/pessoas/:id', PessoaController.atualizar);
 routes.get('/pessoas/:id', PessoaController.detalhePessoa);
 routes.delete('/pessoas/:id', PessoaController.deletar);
+
+// rotas /animais
+routes.post('/animais', AnimalController.criar);
+routes.get('/animais', AnimalController.listarTodos);
+routes.put('/animais/:id', AnimalController.atualizar);
+routes.get('/animais/:id', AnimalController.detalhePessoa);
+routes.delete('/animais/:id', AnimalController.deletar);
 
 export default routes;
