7f77878356776a90feddb89e71d57f7734a388b0
Gustavo Duarte <gduartemenezes@gmail.com>
2020-09-17 20:28:58 -0300

    Adiciona paginas da aplicação

--- gduartemenezes/frontend/src/App.vue
+++ gduartemenezes/frontend/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/detail">Detail</router-link> |
+      <router-link to="/edit">Edit</router-link>
     </div>
     <router-view />
   </div>
---
+++ gduartemenezes/frontend/src/views/Edit.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="edit"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Edit",
+  components: {},
+};
+</script>
