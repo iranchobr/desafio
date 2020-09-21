9e60530f284eefb6f40494bd223629b65df3d701
Gustavo Duarte <gduartemenezes@gmail.com>
2020-09-20 21:47:54 -0300

    Update: adiciona paginacao tbLotes

--- gduartemenezes/frontend/src/components/tables/TableLotes.vue
+++ gduartemenezes/frontend/src/components/tables/TableLotes.vue
@@ -10,7 +10,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="lote in lotes" :key="lote.id">
+        <tr v-for="lote in displayedLotes" :key="lote.id">
           <td>
             <!-- checkbox se limita a uma seleção apenas -->
             <!-- quando clicada emite uma função com dado pessoa a ser usado na importação do componente -->
@@ -45,6 +45,44 @@
         </tr>
       </tbody>
     </table>
+    <div class="offset">
+      <nav class="col-12" aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item">
+            <button
+              type="button"
+              class="page-link"
+              v-if="page != 1"
+              @click="page--"
+            >
+              <b-icon icon="caret-left"></b-icon>
+            </button>
+          </li>
+          <li class="page-item">
+            <button
+              type="button"
+              class="page-link"
+              v-for="pageNumber in pages.slice(page - 1, page + 5)"
+              v-bind:key="pageNumber"
+              @click="page = pageNumber"
+              displayedPessoas
+            >
+              {{ pageNumber }}
+            </button>
+          </li>
+          <li class="page-item">
+            <button
+              type="button"
+              @click="page++"
+              v-if="page < pages.length"
+              class="page-link"
+            >
+              <b-icon icon="caret-right"></b-icon>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -58,49 +96,80 @@ export default {
         {
           id: "7e6e98cc-1413-4aa2-958e-d204326ca591",
           no_lote: "Red",
-          ds_lote:
-            "Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+          ds_lote: "Gado corte",
           created_at: "2020-04-05",
           updated_at: "2020-06-19",
         },
         {
           id: "cc178e85-9911-4bcc-a10a-e86e590518a3",
           no_lote: "Teal",
-          ds_lote:
-            "Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+          ds_lote: "Gado corte",
           created_at: "2019-12-24",
           updated_at: "2020-05-21",
         },
         {
           id: "abe2f382-ed87-49da-87e0-b308f4d7304c",
           no_lote: "Turquoise",
-          ds_lote:
-            "Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+          ds_lote: "Gado corte",
           created_at: "2020-02-03",
           updated_at: "2020-08-19",
         },
         {
           id: "5a36f658-913e-464c-aa36-6de75d58fd29",
           no_lote: "Yellow",
-          ds_lote:
-            "Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+          ds_lote: "Gado corte",
           created_at: "2020-04-20",
           updated_at: "2020-06-11",
         },
         {
           id: "367b3d52-1c58-4ac6-b529-db5972bf04df",
           no_lote: "Mauv",
-          ds_lote:
-            "Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+          ds_lote: "Gado corte",
+          created_at: "2020-03-12",
+          updated_at: "2020-07-28",
+        },
+        {
+          id: "367b3d52-1c58-4ac6-b529-db5972bf04df",
+          no_lote: "Mauv",
+          ds_lote: "Gado corte",
+          created_at: "2020-03-12",
+          updated_at: "2020-07-28",
+        },
+        {
+          id: "367b3d52-1c58-4ac6-b529-db5972bf04df",
+          no_lote: "Mauv",
+          ds_lote: "Gado corte",
+          created_at: "2020-03-12",
+          updated_at: "2020-07-28",
+        },
+        {
+          id: "367b3d52-1c58-4ac6-b529-db5972bf04df",
+          no_lote: "Mauv",
+          ds_lote: "Gado corte",
           created_at: "2020-03-12",
           updated_at: "2020-07-28",
         },
       ],
-
+      page: 1,
+      perPage: 4,
+      pages: [],
       lote: {},
     };
   },
   methods: {
+    setPages() {
+      let numberOfPages = Math.ceil(this.lotes.length / this.perPage);
+      for (let index = 1; index <= numberOfPages; index++) {
+        this.pages.push(index);
+      }
+    },
+    paginate(lotes) {
+      let page = this.page;
+      let perPage = this.perPage;
+      let from = page * perPage - perPage;
+      let to = page * perPage;
+      return lotes.slice(from, to);
+    },
     findLote(id) {
       console.log(id);
     },
@@ -111,15 +180,35 @@ export default {
       console.log(id);
     },
   },
-  created: {
-    buscaLotes() {
-      console.log("buscando");
+  computed: {
+    displayedLotes() {
+      return this.paginate(this.lotes);
     },
+  },
+  created() {
+    this.setPages();
   },
 };
 </script>
 
 <style scoped>
+button.page-link {
+  display: inline-block;
+}
+button.page-link {
+  font-size: 12px;
+  color: #222;
+  font-weight: bold;
+  width: 40px;
+}
+.offset {
+  width: 500px !important;
+  margin: 20px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
 .button-gp {
   display: flex;
   flex-direction: row;
@@ -131,6 +220,8 @@ export default {
 }
 .tb-rendered {
   margin: auto;
+  width: 700px;
+  background: #d070462d;
 }
 
 button {
