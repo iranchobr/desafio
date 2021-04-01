50f5d9e128451e856673d83adf02da496da7f75a
chrystiansantos <chrystian_santos_silva@hotmail.com>
2021-04-01 20:21:21 -0300

    Criando Header.

--- chrystianSantos/front/src/App.tsx
+++ chrystianSantos/front/src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
 import { Header } from './component/Header';
+import GlobalStyle from './styles/Global';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <div style={{ display: 'flex' }}>
+          <Header />
+          <Routes />
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
--- chrystianSantos/front/src/component/Header/index.tsx
+++ chrystianSantos/front/src/component/Header/index.tsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom';
+import { Container } from './styles';
+
 export function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Container>
+      <ul>
+        <li>
+          <Link to="/">Pessoa </Link>
+        </li>
+        <li>
+          <Link to="/animal">Animal</Link>
+        </li>
+        <li>
+          <Link to="/batch">Lote</Link>
+        </li>
+        <li>
+          <Link to="/batch_animal">Animal x lote</Link>
+        </li>
+      </ul>
+    </Container>
   );
 }
