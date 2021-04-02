3de1678c5605effdad576d2df0a628373df13fe7
chrystiansantos <chrystian_santos_silva@hotmail.com>
2021-04-02 02:15:07 -0300

    Criando page de Lotes

--- chrystianSantos/front/src/pages/Batch/index.tsx
+++ chrystianSantos/front/src/pages/Batch/index.tsx
@@ -1,7 +1,47 @@
+import { FiCheckCircle, FiDelete, FiEdit } from 'react-icons/fi';
+import { Input } from '../../component/Input';
+import { Container, Form, Table } from './styles';
+
 export function Batch() {
   return (
-    <div>
-      <h1>Batch</h1>
-    </div>
+    <Container>
+      <h1>Lotes</h1>
+      <Form>
+        <div className="nameDescription">
+          <Input id="name" nameInput="name" nameLabel="Nome" type="text" />
+          <Input
+            id="description"
+            nameInput="description"
+            nameLabel="Descrição"
+            type="text"
+          />
+        </div>
+        <button type="submit">Cadastrar</button>
+      </Form>
+
+      <Table>
+        <thead>
+          <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">Editar/Deletar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Boi brabo</th>
+            <td>So os boi mais brabo</td>
+            <td>
+              <button type="button">
+                <FiEdit size="20" />
+              </button>
+              <button type="button">
+                <FiDelete size="20" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
   );
 }
---
+++ chrystianSantos/front/src/pages/Batch/styles.ts
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  flex: 1;
+  background-color: #eee;
+  h1 {
+    margin: 30px;
+  }
+  max-height: 100vh;
+  overflow: auto;
+`;
+
+export const Form = styled.form`
+  .nameDescription {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 50px;
+    margin: 50px;
+  }
+
+  button {
+    margin-top: 20px;
+    margin-left: 85%;
+    background-color: #92a42a;
+    padding: 10px 15px;
+    border: 0;
+    border-radius: 4px;
+    color: #fff;
+    font-weight: 600;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: rgb(83, 61, 22);
+    }
+  }
+`;
+
+export const Table = styled.table`
+  margin-top: 20px;
+  width: 100%;
+
+  table {
+    border-collapse: collapse;
+  }
+
+  td,
+  th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+    text-align: center;
+  }
+
+  tr:nth-child(even) {
+    background-color: #dddddd;
+    text-align: center;
+  }
+
+  button {
+    border: 0;
+
+    svg {
+      margin-left: 5px;
+    }
+  }
+`;
